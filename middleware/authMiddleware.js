4216db4fbe3020a745c1ac801e58fb50fd837220
mohammadah99123-svg <mohammadah99123@gmail.com>
2026-05-19 09:58:30 +0300

    upload files

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const authMiddleware = (req, res, next) => {
+const authHeader = req.headers.authorization;
+if (!authHeader) {
+return res.status(401).json({
+message: "No Token Provided"
+});
+}
+const token = authHeader.split(" ")[1];
+try {
+const decoded = jwt.verify(token, "secretkey");
+req.user = decoded;
+next();
+} catch (error) {
+return res.status(401).json({
+message: "Invalid Token"
+});
+}
+};
+module.exports = authMiddleware;
